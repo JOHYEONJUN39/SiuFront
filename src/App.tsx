77f7fc219cce043fbefb2e388db1646b865fcfcf
hetame1 <poi8946@naver.com>
2023-09-06 13:08:31 +0900

    ✨ feat: #5 - 헤더 UI 구현, 검색 기능 구현, 검색 결과 페이지 생성
    헤더 ui 구현하고 검색 하고 시간 조금 지났을때 데이터 보내는 식으로 구현함 결과 페이지는 페이지만 만들어놓음

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Nav from './components/Layout/Nav'
+import MainPage from './pages/MainPage'
+import SearchPage from './pages/SearchPage'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Layout = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Nav />
+
+      <Outlet />
     </>
+  )
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
