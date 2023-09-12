8eecb4951756aa555c92da7bb37da17bb38bff6f
hetame1 <poi8946@naver.com>
2023-09-12 17:11:58 +0900

    ✨ feat: #6 #9 - 로그인, 회원가입, 글 api 작성

---
+++ src/api/Board/Post.ts
@@ -0,0 +1,15 @@
+import api from "..";
+
+type PostData = {
+  user_id: string;
+  title: string;
+  article: string;
+  tags: string[];
+}
+
+export const Post = async (data: PostData) => {
+  api.post('/api/createPost', data)
+  .then((res) => {
+    console.log(res);
+  })
+}
---
+++ src/api/Sign/Login.ts
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import api from ".."
+import { setUser } from "../../store/userSlice";
+import { showClose } from "../../store/modalSlice";
+
+type LoginData = {
+  id: string;
+  password: string;
+}
+
+export const useLogin = () => {
+  const dispatch = useDispatch();
+
+  const login = async (data: LoginData) => {
+    await api.get('sanctum/csrf-cookie');
+    console.log("cookie");
+    
+    await api.post('/api/login', data)
+    .then((res) => {
+      dispatch(setUser({
+        id: res.data.id,
+        nickname: res.data.nickname,
+        photo: 'https://cdn-icons-png.flaticon.com/512/3985/3985429.png'
+      }));
+
+      dispatch(showClose());
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  return { login };
+};
