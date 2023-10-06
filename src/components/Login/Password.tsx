08ffd19de4d3df5784e06578a867bac8b84811b4
hetame1 <poi8946@naver.com>
2023-10-07 01:35:28 +0900

    🎨 refactor : 코드 정리

--- src/components/Login/Password.tsx
+++ src/components/Login/Password.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../store'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import styled from "styled-components";
 
 interface Props {
   onInputChange: (value: string) => void;
 }
 
-const PasswordInput = ({onInputChange}: Props) => {
-  const [password, setPassword] = useState<string>("")
-  const [passwordCheck, setPasswordCheck] = useState<string>("")
+const PasswordInput = ({ onInputChange }: Props) => {
+  const [password, setPassword] = useState<string>("");
+  const [passwordCheck, setPasswordCheck] = useState<string>("");
 
-  const [passwordError, setPasswordError] = useState<boolean>(false)
-  const [passwordCheckError, setPasswordCheckError] = useState<boolean>(false)
+  const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [passwordCheckError, setPasswordCheckError] = useState<boolean>(false);
 
-  const type = useSelector((state: RootState) => state.modal.modalType)
+  const type = useSelector((state: RootState) => state.modal.modalType);
 
   // 비밀번호 유효성 검사
   const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,82 +23,76 @@ const PasswordInput = ({onInputChange}: Props) => {
     } = e;
 
     if (name === "password") {
-      onInputChange(value)
-      setPassword(value)
-  
+      onInputChange(value);
+      setPassword(value);
+
       // 로그인 상태면 패스
       if (type === "login") {
         return;
       }
-  
+
       // 유효성 체크
-      const regExp = /^[a-zA-Z0-9]{4,12}$/
+      const regExp = /^[a-zA-Z0-9]{4,12}$/;
       if (!value?.match(regExp)) {
-        setPasswordError(true)
+        setPasswordError(true);
       } else if (passwordCheck.length > 0 && value !== passwordCheck) {
-        setPasswordCheckError(true)
+        setPasswordCheckError(true);
       } else {
-        setPasswordError(false)
+        setPasswordError(false);
       }
-    }
-    else if (name === "passwordCheck") {
-      setPasswordCheck(value)
-  
+    } else if (name === "passwordCheck") {
+      setPasswordCheck(value);
+
       if (value !== password) {
-        setPasswordCheckError(true)
+        setPasswordCheckError(true);
       } else {
-        setPasswordCheckError(false)
+        setPasswordCheckError(false);
       }
     }
-  }
+  };
 
   return (
     <>
       <Password
         name="password"
-        type="password" 
+        type="password"
         placeholder="Password"
         value={password}
         onChange={handlePassword}
         $error={passwordError}
       />
-      {
-        passwordError && (
-          <ErrorText>비밀번호는 4~12자의 영문 대소문자와 숫자로만 입력해주세요.</ErrorText>
-        )
-      }
+      {passwordError && (
+        <ErrorText>
+          비밀번호는 4~12자의 영문 대소문자와 숫자로만 입력해주세요.
+        </ErrorText>
+      )}
 
       {type === "register" && (
         <>
           <RePassword
-            name='passwordCheck'
-            type="password" 
+            name="passwordCheck"
+            type="password"
             placeholder="Password Check"
             value={passwordCheck}
             onChange={handlePassword}
           />
-          {
-            passwordCheckError && (
-              <ErrorText>비밀번호가 일치하지 않습니다.</ErrorText>
-            )
-          }
+          {passwordCheckError && (
+            <ErrorText>비밀번호가 일치하지 않습니다.</ErrorText>
+          )}
         </>
       )}
-
     </>
-    
-  )
-}
+  );
+};
 
-export default PasswordInput
+export default PasswordInput;
 
-
-const Password = styled.input<{$error: boolean}>`
+const Password = styled.input<{ $error: boolean }>`
   width: 300px;
   height: 40px;
   border: none;
   background-color: transparent;
-  border-bottom: 1px solid rgba(255, 255, 255, .2);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   border-radius: 0.2rem;
   margin-bottom: 1rem;
   box-sizing: border-box;
@@ -119,7 +113,9 @@ const Password = styled.input<{$error: boolean}>`
     margin-left: 1rem;
   }
 
-  ${({$error}) => $error && `
+  ${({ $error }) =>
+    $error &&
+    `
     border-bottom: 2px solid red;
 
     &:focus {
@@ -130,14 +126,14 @@ const Password = styled.input<{$error: boolean}>`
       border-bottom: 2px solid red;
     }
   `}
-`
+`;
 
 const RePassword = styled.input`
   width: 300px;
   height: 40px;
   border: none;
   background-color: transparent;
-  border-bottom: 1px solid rgba(255, 255, 255, .2);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   border-radius: 0.2rem;
   margin-bottom: 1rem;
   box-sizing: border-box;
@@ -157,10 +153,10 @@ const RePassword = styled.input`
     font-weight: 100;
     margin-left: 1rem;
   }
-`
+`;
 
 const ErrorText = styled.p`
   color: red;
   font-size: 0.8rem;
   margin-bottom: 1rem;
-`
+`;
