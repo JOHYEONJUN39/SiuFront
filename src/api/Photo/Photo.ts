0b3618909b79055fe2502b335751fe08cb1789cf
NEMURENAIYORU <lhslhs301@gmail.com>
2023-10-16 10:41:18 +0900

    ✨ feat : imagePath를 반환하는 api 작성

--- src/api/Photo/Photo.ts
+++ src/api/Photo/Photo.ts
@@ -1,5 +1,5 @@
 import api from "..";
-import { DeletePhoto, GetPhoto } from "../../types/Photo.interface";
+import { DeletePhoto, GetPath, GetPhoto } from "../../types/Photo.interface";
 
 export const GetImage = async (formData: GetPhoto) => {
   const newImg = await api.post('/api/preview', formData, {
@@ -21,3 +21,10 @@ export const GetImage = async (formData: GetPhoto) => {
   })
   }
   
+  export const GetImagePath = async (photo: GetPath) => {
+    return await api.post("/api/posts/storeImage", photo, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+  };
